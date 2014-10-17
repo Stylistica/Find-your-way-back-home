b29ebd2a53b2db869e69a9e9c5128c763f6c8b59
Stylistica <24.sneha@gmail.com>
2014-10-17 11:43:30 +0530

    Update Main.java
    Formatted Main file

--- Main.java
+++ Main.java
@@ -15,45 +15,44 @@ class WayBackHome
 		
 		Scanner in = new Scanner(System.in);
  
-    	System.out.println("Enter the number of directions to destination:");
-    	int size = in.nextInt();
-    	System.out.println("Enter the directions to destination:\n");
-    	for(int i=0;i<=size;i++){
-    		routes.add(in.nextLine());
-    	}
-    	routes.remove(0);
-    
+	    	System.out.println("Enter the number of directions to destination:");
+	    	int size = in.nextInt();
+	    	System.out.println("Enter the directions to destination:\n");
+	    	for(int i=0;i<=size;i++){
+	    		routes.add(in.nextLine());
+    		}
+    		routes.remove(0);
 		for (String route : routes) {
 			directions.add(route.substring(0, route.indexOf(" ")));
 			places.add(route.substring(route.indexOf(" ")));
 		}
 		
-	Collections.reverse(places);
+		Collections.reverse(places);
 	
 	
-	directions.remove("Begin");
-	for (int i = 0; i < directions.size(); i++) {
-		if(directions.get(i).equals("Left"))
-		{
-			
-			directions.set(i, "Right");
-			
+		directions.remove("Begin");
+		for (int i = 0; i < directions.size(); i++) {
+			if(directions.get(i).equals("Left"))
+			{
+				
+				directions.set(i, "Right");
+				
+			}
+			else if(directions.get(i).equals("Right"))
+			{
+				directions.set(i, "Left");
+				
+			}
 		}
-		else if(directions.get(i).equals("Right"))
-		{
-		directions.set(i, "Left");
-			
-		}
-	}
 
-	Collections.reverse(directions);
-	directions.add(0,"Begin");
+		Collections.reverse(directions);
+		directions.add(0,"Begin");
 	
-	System.out.println("\nFollow the below route to find your way back home:");
-	System.out.println("\n");
-	for (int i = 0; i < directions.size(); i++) {
-		System.out.println((i+1)+"."+directions.get(i)+places.get(i));
+		System.out.println("\nFollow the below route to find your way back home:");
+		System.out.println("\n");
+		for (int i = 0; i < directions.size(); i++) {
+			System.out.println((i+1)+"."+directions.get(i)+places.get(i));
+		}
+	
 	}
-	
-}
 }
